d6d1e70b15e6c3dc9cb2869e75855795a77d51c8
taomaru <jackson_maria1@columbusstate.edu>
2022-07-01 17:40:08 -0400

    Pull key value for clicks and keyboard presses of calc btns

--- main.js
+++ main.js
@@ -25,3 +25,64 @@ function addOpClickStyle(e) {
 function removeOpClickStyle(e) {
     e.target.classList.remove("clickedOperator");
 }
+
+//Calculator functions:
+const add = function(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+  
+const subtract = function(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+};
+
+const multiply = function(num1, num2) {
+    let product = num1 * num2;
+    return product;
+};
+
+const divide = function(num1, num2) {
+    let quotient = num1 / num2;
+    return quotient;
+};
+
+/*
+const operate = function(num1, num2, operation) {
+    switch(operation) {
+        case "+" : return add(num1, num2);
+            break;
+        case "-" : return subtract(num1, num2);
+            break;
+        case "*" : return multiply(num1, num2);
+            break;
+        case "/" : return divide(num1, num2);
+            break;
+    }
+};
+*/
+
+//Operation variables, event listeners:
+const numPad = document.querySelector(".numberPadContainer");
+numPad.addEventListener("click", findKey);
+
+const operatorPad = document.querySelector(".operatorContainer");
+operatorPad.addEventListener("click", findKey);
+
+const displayArea = document.querySelector(".calcDisplay");
+
+window.addEventListener( "keydown", findPressedKey);
+
+function findKey(e) {
+    //console.log(e.target.attributes["data-key"]);
+    console.log(e.target.dataset.key);
+};
+
+function findPressedKey(e) {
+    //console.log(e);
+    let pressedKey = document.querySelector(`div[data-key="${e.key}"`);
+    //let displayText = document.createElement("p");
+    //displayArea.appendChild(displayText);
+    //displayText.textContent = pressedKey;
+    console.log(pressedKey.dataset.key);
+};
